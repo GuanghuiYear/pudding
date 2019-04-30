f62cd6b228b65539708906c51be8c54b42187848
GuanghuiYear <956375089@qq.com>
2019-04-30 13:58:05 +0800

    轨迹设置

--- pages/profile/viewtrace.js
+++ pages/profile/viewtrace.js
@@ -1,66 +1,56 @@
-// pages/profile/viewtrace.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    date: '2018-01-02',
+    time: '12:00'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
+  bindDateChange_start() {
+    var date = new Date();
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    var monthDay = ['今天', '明天'];
+    var hours = [];
+    var minute = [];
 
+    // 月-日
+    for (var i = 2; i <= 28; i++) {
+      var date1 = new Date(date);
+      date1.setDate(date.getDate() + i);
+      var md = (date1.getMonth() + 1) + "-" + date1.getDate();
+      monthDay.push(md);
+    }
+
+    // 时
+    for (var i = 0; i < 24; i++) {
+      hours.push(i);
+    }
+
+    // 分
+    for (var i = 0; i < 60; i += 10) {
+      minute.push(i);
+    }
+
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    data.multiArray[0] = monthDay;
+    data.multiArray[1] = hours;
+    data.multiArray[2] = minute;
+    this.setData(data);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindTimeChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
   }
 })
