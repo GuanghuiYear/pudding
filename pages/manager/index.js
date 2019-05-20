fe44888b29260ffa83c927160ccfcaaa1d50eed2
GuanghuiYear <956375089@qq.com>
2019-05-20 10:56:37 +0800

    距离优化
    Signed-off-by: GuanghuiYear <956375089@qq.com>

--- pages/manager/index.js
+++ pages/manager/index.js
@@ -2,23 +2,23 @@ var util = require('../../utils/util.js');
 var app = getApp();
 Page({
   data: {
-    members:[],
-    isEmptyPageDesc:'正在加载...',
+    members: [],
+    isEmptyPageDesc: '正在加载...',
     isEmptyPage: true
   },
 
-  itemTouch:function(evt){
+  itemTouch: function(evt) {
     wx.navigateTo({
       url: '../../pages/manager/personprofile',
     })
   },
 
-  requestMembers:function(callback){
+  requestMembers: function(callback) {
     let gb = app.globalData
-    if(!gb.selGroup){
+    if (!gb.selGroup) {
       this.setData({
         isEmptyPageDesc: '您当前没有加入任何组织,请联系管理员。',
-        isEmptyPage:true
+        isEmptyPage: true
       })
       return;
     }
@@ -33,7 +33,7 @@ Page({
 
     let that = this
     wx.showLoading();
-    util.httpGet(url, function (res) {
+    util.httpGet(url, function(res) {
       wx.hideLoading()
       let tmpMems = res.data
       if (!tmpMems || tmpMems.length == 0) {
@@ -42,7 +42,13 @@ Page({
           isEmptyPageDesc: '当前账户无可管理成员'
         })
       } else {
-        console.log(tmpMems)
+        for (let i = 0; i < tmpMems.length; i++) {
+          if (tmpMems[i].distance == 12395342) {
+            tmpMems[i].distance = '无信号';
+          } else {
+            tmpMems[i].distance = tmpMems[i].distance + '米';
+          }
+        }
         that.setData({
           members: tmpMems,
           isEmptyPage: false,
@@ -52,27 +58,28 @@ Page({
     })
   },
 
-  phoneCallHandler:function(evt){
-    if(!evt.currentTarget.dataset.mobile){
+  phoneCallHandler: function(evt) {
+    if (!evt.currentTarget.dataset.mobile) {
       wx.showModal({
         content: '无法呼叫该用户，手机号未找到.',
         showCancel: false
       })
     } else {
-      wx.makePhoneCall({phoneNumber:evt.currentTarget.dataset.mobile})  
+      wx.makePhoneCall({
+        phoneNumber: evt.currentTarget.dataset.mobile
+      })
     }
   },
 
-  onLoad: function (options) {
-  },
+  onLoad: function(options) {},
 
-  onReady: function () {},
-  onShow: function () {
+  onReady: function() {},
+  onShow: function() {
     this.requestMembers()
   },
-  onHide: function () {},
-  onUnload: function () {},
-  onPullDownRefresh: function () {},
-  onReachBottom: function () {},
-  onShareAppMessage: function () {}
+  onHide: function() {},
+  onUnload: function() {},
+  onPullDownRefresh: function() {},
+  onReachBottom: function() {},
+  onShareAppMessage: function() {}
 })
